b94c3a39688ddd29c833ea1d30afdc740bf767a2
julien AWONGA <84460584+julienawonga@users.noreply.github.com>
2024-02-03 15:01:58 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Julien AWON'GA",
+  title: "Data/ML Engineer & Cloud Architect",
+  email: "julienawon@gmail.com",
+  gitHub: "julienawonga",
+  instagram: "julienawonga",
+  linkedIn: "julienawonga",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "julienawonga",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
